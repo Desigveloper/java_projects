5dd49f27626784d167d67cdc0368f3507a1e05ad
desigveloper <devhabib60@gmail.com>
2024-08-25 07:57:32 +0000

    Updated and optimised calculator.java file

--- projects/calculator/Calculator.java
+++ projects/calculator/Calculator.java
@@ -1,36 +1,129 @@
 package projects.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
     private double result;
+    private double firstNum;
+    private double secondNum;
+    private char op;
 
-    Calculator(double num1, char op, double num2) {
+    Calculator() {
+    
+    }
+
+    private void doCalculation(double firstNum, char op, double secondNum) {
         switch (op) {
             case '+':
-                result = num1 + num2;
+                setResult(firstNum + secondNum);
                 break;
             case '-':
-                result = num1 - num2;
+                setResult(firstNum - secondNum);
                 break;
             case '*':
-                result = num1 * num2;
+                setResult(firstNum * secondNum);
                 break;
             case '/':
-                result = num1 / num2;
+                setResult(firstNum / secondNum);
                 break;
             case '%':
-                result = num1 % num2;
+                setResult(firstNum % secondNum);
                 break;
             default:
                 break;
         }
     }
 
-    double getResult() {
+    public void getInputs() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Operator +, -, *, /, %:");
+        setOp(sc.next().charAt(0));
+
+        System.out.println("Enter the first number:");
+        setFirstNum(sc.nextDouble());
+        
+        System.out.println("Enter the second number:");
+        setSecondNum(sc.nextDouble());
+
+        sc.close();
+
+    }
+
+    private void setFirstNum(double num) {
+        this.firstNum = num;
+    }
+    private double getFirstNum() {
+        return firstNum;
+    }
+
+    
+    private void setSecondNum(double num) {
+        this.secondNum = num;
+    }
+    private double getSecondNum() {
+        return secondNum;
+    }
+
+    private void setOp(char op) {
+        this.op = op;
+    }
+    private char getOp() {
+        return op;
+    }
+
+    private void setResult(double result) {
+        this.result = result;
+    }
+
+    private double getResult() {
         return result;
     }
 
-
-    boolean checkDecimal(double result) {
+    private boolean checkDecimal(double result) {
         return result != Math.floor(result);
+    }
+
+    private int checkNumbers() {
+        double num1 = getFirstNum();
+        double num2 = getSecondNum();
+        long intNum1 = Math.round(num1);
+        long intNum2 = Math.round(num2);
+        
+        if (intNum1 == num1 && intNum2 == num2)
+            return 0;
+        else if (intNum1 == num1 && intNum2 != num2)
+            return 1;
+        else if (intNum1 != num1 && intNum2 == num2)
+            return 2;
+        return 3;
+    }
+
+    public void printResult() {
+        double num1 = getFirstNum();
+        double num2 = getSecondNum();
+        char operator = getOp();
+
+        doCalculation(num1, operator, num2);
+
+        double doubleValue = getResult();
+        boolean checkResult = checkDecimal(doubleValue);
+        
+        if (checkResult) {
+            if (checkNumbers() == 0)
+                System.out.println(Math.round(num1) + " " + operator + " " + Math.round(num2) + " = " + doubleValue);
+            else if (checkNumbers() == 1)
+                System.out.println(Math.round(num1) + " " + operator + " " + num2 + " = " + doubleValue);
+            else if (checkNumbers() == 2)
+                System.out.println(num1 + " " + operator + " " + Math.round(num2) + " = " + doubleValue);
+            else
+                System.out.println(num1 + " " + operator + " " + num2 + " = " + doubleValue);
+        }
+        else {
+            long intResult = Math.round(doubleValue);
+            long intNum1 = Math.round(num1);
+            long intNum2 = Math.round(num2);
+            System.out.println(intNum1 + " " + op + " " + intNum2 + " = " + intResult);
+        }
     }
 }
