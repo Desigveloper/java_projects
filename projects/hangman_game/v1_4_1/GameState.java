474c699e9df60e5227622d8633cb46899672dbad
Desigveloper <Devhabib60@gmail.com>
2024-09-29 09:50:54 +0000

    rebuit and packaged hangman game

--- projects/hangman_game/v1_4_1/GameState.java
+++
@@ -1,7 +0,0 @@
-package projects.hangman_game.v1_4_1;
-
-import java.util.HashSet;
-public abstract class GameState {
-    public void  displayGameState(char[] currentGuess, int remainingGuesses, HashSet<Character> guessedLetters,
-                int hintsRemaining, int secondsLeft){};
-}
--- projects/hangman_game/v1_4_2/GameState.java
+++
@@ -1,9 +0,0 @@
-package projects.hangman_game.v1_4_2;
-
-import java.util.HashSet;
-public abstract class GameState {
-    public void  displayGameState(char[] currentGuess, int remainingGuesses, HashSet<Character> guessedLetters,
-                int hintsRemaining, int secondsLeft){};
-
-    protected void endGame(){};
-}
